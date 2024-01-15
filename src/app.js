ff41d31de302688d7cbb4ed1a2f4fb1246984f0b
Ale <AROrtega@hotmail.com>
2024-01-14 22:27:17 -0300

    Handlebars instalado y configurado. Creado public, con su .css y script main. Nuevo router para handlebars, views y ajuste de codigo

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const app = express();
+const exphbs = require("express-handlebars");
 const PUERTO = 8080;
 
 const productsRouter = require("./routes/products.router");
 const cartsRouter = require("./routes/carts.router.js");
+const viewsRouter = require("./routes/views.router.js");
 
+//Middlewares
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static("./src/public"));
 
+//CFG Handlebars
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
+//Routes
 app.use("/api", productsRouter);
 app.use("/api", cartsRouter);
+app.use("/", viewsRouter);
 
+//Listen
 app.listen(PUERTO, () => {
     console.log(`Escuchando en http://localhost:${PUERTO}`);
 });
